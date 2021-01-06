b96256f314e38ce1b0813028bfcc48b4645ff411
Devora Nagel <devoranagel@gmail.com>
2021-01-06 14:31:17 -0600

    added rest of shapes

--- src/main/java/tetris/Tetromino.java
+++ src/main/java/tetris/Tetromino.java
@@ -19,6 +19,15 @@ public class Tetromino {
 
     public Tetromino() {
         createOShape();
+        createIShape();
+        createJShape();
+        createLShape();
+        createSShape();
+        createZShape();
+        createTShape();
+        createPShape();
+        createUShape();
+        createMiniShape();
     }
 
     private void createOShape() {
@@ -27,8 +36,113 @@ public class Tetromino {
         for (int i = 0; i < 4; i++) {
             oShape.add(new Square(x+i, y));
             if(i == 1) {
+                x = x - (i+1);
                 y = y - 1;
             }
+        }
+    }
+
+    private void createIShape() {
+        int x = Grid.WIDTH/2;
+        int y = Grid.HEIGHT;
+        for (int i = 0; i < 4; i++) {
+            iShape.add(new Square(x + i, y));
+        }
+    }
+
+    private void createJShape() {
+        int x = Grid.WIDTH/2;
+        int y = Grid.HEIGHT;
+        for (int i = 0; i < 4; i++) {
+            jShape.add(new Square(x, y-1));
+            if(i == 2) {
+                x = x - 1;
+                y = y + 1;
+            }
+        }
+    }
+
+    private void createLShape() {
+        int x = Grid.WIDTH/2;
+        int y = Grid.HEIGHT;
+        for (int i = 0; i < 4; i++) {
+            lShape.add(new Square(x, y-1));
+            if(i == 2) {
+                x = x + 1;
+                y = y + 1;
+            }
+        }
+    }
+
+    private void createSShape() {
+        int x = Grid.WIDTH/2;
+        int y = Grid.HEIGHT;
+        for (int i = 0; i < 4; i++) {
+            sShape.add(new Square(x+i, y));
+            if(i == 1) {
+                x = x - (i + 2);
+                y = y - 1;
+            }
+        }
+    }
+
+    private void createZShape() {
+        int x = Grid.WIDTH/2;
+        int y = Grid.HEIGHT;
+        for (int i = 0; i < 4; i++) {
+            zShape.add(new Square(x+i, y));
+            if(i == 1) {
+                x = x - i;
+                y = y - 1;
+            }
+        }
+    }
+
+    private void createTShape() {
+        int x = Grid.WIDTH/2;
+        int y = Grid.HEIGHT;
+        for (int i = 0; i < 4; i++) {
+            tShape.add(new Square(x + i, y));
+            if (i == 2) {
+                x = x - i;
+                y = y - 1;
+            }
+        }
+    }
+
+    private void createPShape() {
+        int x = Grid.WIDTH/2;
+        int y = Grid.HEIGHT;
+        for (int i = 0; i < 5; i++) {
+            pShape.add(new Square(x+i, y));
+            if(i == 2) {
+                x = x - i;
+                y = y - 1;
+            }
+        }
+    }
+
+    private void createUShape() {
+        int x = Grid.WIDTH/2;
+        int y = Grid.HEIGHT;
+        for (int i = 0; i < 7; i++) {
+            uShape.add(new Square(x+i, y));
+            if(i == 2) {
+                x = x - i;
+                y = y - 1;
+            }
+            if (i == 3) {
+                x = x - i;
+                y = y - 1;
+            }
+        }
+    }
+
+    private void createMiniShape() {
+        int x = Grid.WIDTH/2;
+        int y = Grid.HEIGHT;
+        for (int i = 0; i < 2; i++) {
+            miniShape.add(new Square(x + i, y));
         }
     }
 
