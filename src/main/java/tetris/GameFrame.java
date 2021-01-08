bd479c5506141b0a5b92b9e3183e74b326276677
Devora Nagel <devoranagel@gmail.com>
2021-01-08 12:28:09 -0600

    updated view and frame

--- src/main/java/tetris/GameFrame.java
+++ src/main/java/tetris/GameFrame.java
@@ -1,14 +1,45 @@
 package tetris;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GameFrame extends JFrame {
     private Grid grid;
-    private JPanel buttonPanel;
+    private GridView gridView;
+    private JPanel panel;
     private JPanel upcomingShapePanel;
     private JButton play;
+    private int delay = 200;
 
-    public GameFrame() {
+    public GameFrame(Grid grid, GridView gridView) {
+        this.grid = grid;
+        this.gridView = gridView;
 
+        setSize(420, 500);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Tetris");
+        setLayout(new BorderLayout());
+
+        play = new JButton("Play \u25B6");
+        play.addActionListener(actionEvent -> playLoop());
+        panel.add(play);
+
+        add(gridView);
+        add(panel, BorderLayout.EAST);
+    }
+
+    private void playLoop() {
+        Thread thread = new Thread(() -> {
+            while(true) {
+                grid.lowerShape();
+                gridView.repaint();
+                try {
+                    Thread.sleep(delay);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
     }
 }
--- src/main/java/tetris/GridView.java
+++ src/main/java/tetris/GridView.java
@@ -5,10 +5,8 @@ import java.awt.*;
 
 public class GridView extends JComponent {
     private Grid grid;
-    private static final int GRID_SIZE = 500;
-    private static final int CELL_SIZE = 20;
-    public static final int rows = GRID_SIZE/CELL_SIZE;
-    public static final int cols = GRID_SIZE/CELL_SIZE;
+    private Tetromino tetro;
+    private static final int CELL_SIZE = 15;
     
     public GridView(Grid grid) {
         this.grid = grid;
@@ -18,8 +16,22 @@ public class GridView extends JComponent {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         paintGrid(g);
+        paintCurrShape(g);
     }
 
     private void paintGrid(Graphics g) {
+        boolean board[][] = grid.getGrid();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                g.setColor(Color.BLACK);
+                g.drawRect(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+            }
+        }
+    }
+
+    private void paintCurrShape(Graphics g) {
+        if (tetro.getCurrentShape() != null) {
+
+        }
     }
 }
