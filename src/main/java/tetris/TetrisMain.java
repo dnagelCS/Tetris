bcaeeb8641c447c0140db41a185e3368e58eb9b8
Tamar <tamartda@gmail.com>
2021-01-11 17:05:14 -0500

    added main

--- src/main/java/tetris/TetrisMain.java
+++ src/main/java/tetris/TetrisMain.java
@@ -2,8 +2,12 @@ package tetris;
 
 public class TetrisMain {
     public static void main(String[] args) {
-        Grid grid = new Grid();
+        Tetromino tetromino = new Tetromino();
+        Grid grid = new Grid(tetromino);
         GridView view = new GridView(grid);
+        TetrisKeyListener keyListener = new TetrisKeyListener();
+        GridThread gridThread = new GridThread(grid, view);
+        thread.start();
         new GameFrame(grid,view).setVisible(true);
     }
 }
