fd8953d934eec62181c86947ea39575bfc9d9935
Tamar <tamartda@gmail.com>
2021-01-10 23:04:29 -0500

    added thread for grid

---
+++ src/main/java/tetris/GridThread.java
@@ -0,0 +1,45 @@
+package tetris;
+
+public class GridThread {
+
+        private static final int DELAY_MS = 80;
+        private int currentDelay;
+        private int currentRound;
+
+        private final Grid grid;
+        private final GridView gridView;
+
+        public GridThread(Grid grid, GridView gridView) {
+            this.grid = grid;
+            this.gridView = gridView;
+            currentDelay = DELAY_MS;
+            currentRound = 0;
+        }
+
+        /**
+         * Every second, lower shape and repaint the grid
+         */
+        public void run() {
+            while (gameIsPlaying()) {
+                grid.lowerShape();
+                gridView.repaint();
+                try {
+                    decrementDelay();
+                    Thread.sleep(currentDelay);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        public void decrementDelay() {
+            if (currentDelay > 15) {
+                if (currentRound < 50) {
+                    currentRound++;
+                } else {
+                    currentDelay--;
+                    currentRound = 0;
+                }
+            }
+        }
+    }
