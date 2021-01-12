0e52d43df335143a114d5d90bc1038f0bd64f37e
Tamar <tamartda@gmail.com>
2021-01-12 11:58:10 -0500

    worked on gridView

--- src/main/java/tetris/GridView.java
+++ src/main/java/tetris/GridView.java
@@ -1,11 +1,14 @@
 package tetris;
 
+import tetris.shapes.AllShapes;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class GridView extends JComponent {
     private Grid grid;
-    private static final int CELL_SIZE = 15;
+    private static final int CELL_SIZE = 10;
     
     public GridView(Grid grid) {
         this.grid = grid;
@@ -15,19 +18,41 @@ public class GridView extends JComponent {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         paintGrid(g);
+        paintFixedSquares(g);
         paintCurrShape(g);
     }
 
     private void paintGrid(Graphics g) {
-        boolean board[][] = grid.getGrid();
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                g.setColor(Color.BLACK);
-                g.drawRect(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+        g.setColor(Color.LIGHT_GRAY);
+
+        for (int i = 0; i < Grid.ROWS; i++) {
+            g.drawLine(0, i * Grid.CELL_SIZE, Grid.WIDTH, i * Grid.CELL_SIZE);
+        }
+
+        for (int i = 0; i < Grid.COLS; i++) {
+            g.drawLine(i * CELL_SIZE, 0, i * Grid.CELL_SIZE, Grid.HEIGHT);
+        }
+    }
+
+    private void paintFixedSquares(Graphics g){
+        Square[][] fixedSquares = grid.getFixedSquares();
+        for (int row = 1; row < Grid.ROWS; row++) {
+            for (int col = 0; col < Grid.COLS; col++) {
+                if (fixedSquares[row][col] != null) {
+                    g.setColor(fixedSquares[row][col].getColor());
+                    g.fillRect(fixedSquares[row][col].getX() * CELL_SIZE, fixedSquares[row][col].getY() * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                }
             }
         }
     }
 
     private void paintCurrShape(Graphics g) {
+        AllShapes shape = grid.getCurrentShape();
+        ArrayList<Square> squares = shape.getSquares();
+        for(Square square : squares)
+        {
+            g.setColor(square.getColor());
+            g.fillRect(square.getX() * CELL_SIZE, square.getY() * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+        }
     }
 }
